7135cb488c5cd8a30e664955bf69e496476c8888
Samuel ODaniels <samodan195@gmail.com>
2022-06-16 02:26:06 +0100

    Write #build_tree for Tree class

--- tree.rb
+++ tree.rb
@@ -1,9 +1,23 @@
 #frozen_string_literal: true
 
+require_relative './node'
+
 #creates trees
 class Tree
   def initialize src_array
     @root = nil
     @scr_array = scr_array
   end
+
+  def build_tree(arr, start, ending)
+    return nil if start > ending
+
+    mid = (start + ending) / 2
+    node = Node.new([arr[mid]])
+
+    node.left = build_tree(arr, start, mid - 1)
+    node.right = build_tree(arr, mid + 1, ending)
+    node
+  end
+
 end
