da799dea2a40174d4ccc0e150b063b18c8d36731
Samuel ODaniels <samodan195@gmail.com>
2022-06-19 02:04:55 +0100

    Add 1 sec delay between executions in driver script

--- main.rb
+++ main.rb
@@ -5,38 +5,49 @@ require_relative './tree'
 # Test drive script
 puts "building new tree.."
 my_tree = Tree.new(Array.new(15) { rand(1..100) })
-puts "balanced: #{my_tree.balanced?}"
+sleep 1
+puts "balanced?: #{my_tree.balanced?}"
+sleep 1
 puts 'level-order:'
 my_tree.level_order { |n| print "#{n.data} " }
 puts "\n"
+sleep 1
 puts 'preorder:'
 my_tree.preorder { |n| print "#{n.data} " }
 puts "\n"
+sleep 1
 puts 'postorder:'
 my_tree.postorder { |n| print "#{n.data} " }
 puts "\n"
+sleep 1
 puts 'in-order:'
 my_tree.in_order { |n| print "#{n.data} " }
 puts "\n"
-
-puts 'insert some nodes to umbalance the tree'
+sleep 1
+puts 'inserting some nodes to umbalance the tree'
 8.times { |num| my_tree.insert(101 + num) }
+sleep 1
 puts "balanced: #{my_tree.balanced?}"
-
-puts 'rebalance tree...'
+sleep 1
+puts 'rebalancing tree...'
 my_tree.rebalance
-
-puts "balanced: #{my_tree.balanced?}"
-
+sleep 1
+puts "balanced?: #{my_tree.balanced?}"
+sleep 1
 puts 'level-order:'
 my_tree.level_order { |n| print "#{n.data} " }
 puts "\n"
+sleep 1
 puts 'preorder:'
 my_tree.preorder { |n| print "#{n.data} " }
 puts "\n"
+sleep 1
 puts 'postorder:'
 my_tree.postorder { |n| print "#{n.data} " }
 puts "\n"
+sleep 1
 puts 'in-order:'
 my_tree.in_order { |n| print "#{n.data} " }
 puts "\n"
+
+puts 'test completed...'
